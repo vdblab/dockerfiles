7654282b518ba0bd49ccb71e28b527b67a3c555c
Nick Waters <nickp60@users.noreply.github.com>
2022-12-19 13:49:01 -0500

    bump bowtie and include pigz

---
+++ test.sh
@@ -0,0 +1,22 @@
+#! /bin/bash
+set -eux
+set -o pipefail
+
+image=$1
+base="${image%:*}"
+tag="${image#*:}"
+docker build -t $image  $base/
+
+case $base in
+
+  bowtie2 )
+      docker run --rm $image pigz -h
+      docker run --rm $image bowtie2 -h
+      docker run --rm $image samtools
+      docker run --rm $image bedtools -h
+
+      ;;
+  *)
+      echo "Invalid option. Exiting"
+      ;;
+esac
